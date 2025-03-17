cbb0834e4548fb72c17b128fe019427959c71a41
hoaiphuongtr <ctbt2004@gmail.com>
2025-03-17 23:45:38 +0700

    Complete navbar section

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#mobile_menu');
+const menuLinks = document.querySelector('.navbar_menu');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
